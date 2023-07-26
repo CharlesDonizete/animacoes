f6fd7bfea966faa326a05a1120d4d9c2680972d6
CharlesDonizete <82051427+CharlesDonizete@users.noreply.github.com>
2023-07-26 18:11:32 -0300

    05 - Desafio check

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -52,3 +52,14 @@ export const shownStateTrigger = trigger('showState', [
     ),
   ]),
 ]);
+
+export const checkButtonTrigger = trigger('checkButton', [
+  transition('* => checked', [
+    animate(
+      '400ms ease-in',
+      style({
+        transform: 'scale(0.4)',
+      })
+    ),
+  ]),
+]);
